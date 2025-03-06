443cc471a32f309dac30fa36a0c6a5638aa31c9b
Daniel Trujillo <danny@MacBook-Pro-de-Daniel.local>
2025-03-06 10:11:02 -0600

    Fomulario Y cards

--- PetPawsHN/vite.config.js
+++ PetPawsHN/vite.config.js
@@ -1,5 +1,33 @@
-import { defineConfig } from 'vite'
+import path, {resolve} from "node:path";
+import { defineConfig } from 'vite';
+import * as glob from 'glob';
+import htmlPurge from 'vite-plugin-purgecss';
+
+const  obtenerEntradasHTML = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob
+                .sync('./**/*.html', {ignore: ["./dist/**", "./node_modules/**"]}
+
+                ).map(
+                    fileData.slice(0, fileData.length - path.extname(fileData).length),
+                    resolve(__dirname, fileData)
+                )
+        ]
+    );
+}
 
 export default defineConfig({
-    appType: 'mpa'
+    appType: 'mpa',
+    base: process.env.DEPLOY_BASE_URL,
+    build: {
+        rollupOptions: {
+            input: obtenerEntradasHTML()
+        }
+    },
+    plugins: [
+        htmlPurge({})
+    ]
+
+
 });
